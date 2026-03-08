95f85fa4f947bbd48932a1d10aa80b1ec7535aa6
Davidak <davidgames.youbtub@gmail.com>
2026-03-08 12:14:24 +0100

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
-qemu-system-i386 -debugcon stdio -machine q35 \
--hda build/diskimage.dd
+qemu-system-i386 \
+  -debugcon stdio \
+  -machine q35,smbus=off \
+  -cpu pentium3 \
+  -m 128M \
+  -nodefaults \
+  -device ich9-ahci,id=ahci \
+  -drive file=build/diskimage.dd,id=disk0,format=raw,if=none \
+  -device ide-hd,drive=disk0,bus=ahci.0 \
+  -vga std
